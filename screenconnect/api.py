3a144ba70560bafa760a9b7055c03659376caea2
Jacob Turpin <JTurpin@ScreenConnect.com>
2016-05-09 10:16:39 -0400

    added api and session classes

---
+++ screenconnect/api.py
@@ -0,0 +1,9 @@
+''' A library that provides a Python interface to the ScreenConnect API '''
+
+
+class ScreenConnect():
+    ''' A python interface into the ScreenConnect API '''
+
+    def __init__(self, **kwargs):
+        ''' Instantiate a new ScreenConnect object '''
+        pass
---
+++ screenconnect/session.py
@@ -0,0 +1,9 @@
+
+
+
+class Session():
+    ''' Object for interacting with ScreenConnect Sessions '''
+
+    def __init__(self, **kwargs):
+        ''' Instantiates a new session object '''
+        pass
