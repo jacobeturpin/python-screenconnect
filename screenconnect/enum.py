89be19b9e7d415dfd9ad4707b60b552ce64aa2a9
Jacob Turpin <JTurpin@ScreenConnect.com>
2016-05-09 19:49:43 -0400

    added skeleton methods to modify/end sessions and added enumerations file

---
+++ screenconnect/enum.py
@@ -0,0 +1,8 @@
+from enum import Enum
+
+class SessionEvent(Enum):
+    pass
+
+
+class SessionConnectionEvent(Enum):
+    pass
--- screenconnect/session.py
+++ screenconnect/session.py
@@ -7,3 +7,10 @@ class Session():
     def __init__(self, **kwargs):
         ''' Instantiates a new session object '''
         pass
+
+    def modify_session_details(self, **kwargs):
+        ''' Alter the details or settings of the session '''
+        pass
+
+    def end(self):
+        pass
