f7c1eb9966e36644dbe354b435c24e72b88a66d2
jacobeturpin <jacobeturpin@gmail.com>
2016-05-09 17:20:15 -0400

    Added base class for errors

---
+++ screenconnect/error.py
@@ -0,0 +1,9 @@
+
+
+class ScreenConnectError(Exception):
+    ''' Base class for ScreenConnect errors '''
+
+    @property
+    def message(self):
+        ''' Returns provided message to construct error '''
+        return self.args[0]
