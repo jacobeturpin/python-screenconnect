7e08e33aa7b2106a1b030b4c9e8d1aa636748f83
Jacob Turpin <JTurpin@ScreenConnect.com>
2016-05-09 12:09:48 -0400

    began working on authentication for api object

--- screenconnect/api.py
+++ screenconnect/api.py
@@ -1,9 +1,26 @@
 ''' A library that provides a Python interface to the ScreenConnect API '''
 
+import urllib
 
 class ScreenConnect():
     ''' A python interface into the ScreenConnect API '''
 
-    def __init__(self, **kwargs):
-        ''' Instantiate a new ScreenConnect object '''
-        pass
+    def __init__(self,
+                 url,
+                 auth = None):
+        ''' Instantiate a new ScreenConnect object 
+        
+        url = publicly accessible url for your ScreenConnect web server 
+        auth = (user, pwd)
+        '''
+
+        self.url = url
+        self.user, self.pwd = auth
+
+    def _get_authentication_cookie(self):
+        ''' Captures and stored the authentication cookie for specified
+        user '''
+
+        # Need to resolve
+        r = urllib.request.urlopen(self.url)
+        self._auth_cookie = r.getheader('Set-Cookie')
