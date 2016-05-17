a768ed9f9a67e6ac5127fe4bc5f95a440f364d5c
jacobeturpin <jacobeturpin@gmail.com>
2016-05-17 18:28:54 -0400

    Implemented lambda expression to populate SessionGroup instance variables by kwargs

--- screenconnect/session_group.py
+++ screenconnect/session_group.py
@@ -1,9 +1,8 @@
-
+from enumerations import SessionType
 
 class SessionGroup():
 
-    def __init__(self, api, name, session_type, is_system, session_filter,
-                 subgroup_expressions):
+    def __init__(self, api, **kwargs):
         ''' Instantiates a session group object 
         
         Arguments:
@@ -15,13 +14,15 @@ class SessionGroup():
         subgroup_expressions -- criteria for any further session organization
         '''
 
-        self.api = api
-        self.name = name
-        self.session_type = session_type
+        f = lambda x: kwargs.get(x)
 
-        self.is_system = is_system
-        self.session_filter = session_filter
-        self.subgroup_expressions = subgroup_expressions
+        self.api = api
+
+        self.name = f('Name')
+        self.session_type = SessionType(f('SessionType'))
+        self.is_system = f('IsSystem')
+        self.session_filter = f('SessionFilter')
+        self.subgroup_expressions = f('SubgroupExpressions')
 
     def modify(self, **kwargs):
         ''' Alter the details or settings of the session group '''
