78e7964e200408ac6c53e9b1494db2499c5174f3
Jacob Turpin <JTurpin@ScreenConnect.com>
2016-05-17 09:30:15 -0400

    added parameters for SessionGroup __init__ method

--- screenconnect/session_group.py
+++ screenconnect/session_group.py
@@ -2,12 +2,26 @@
 
 class SessionGroup():
 
-    def __init__(self, api, name, session_type):
-        ''' Instantiates a session group object '''
+    def __init__(self, api, name, session_type, is_system, session_filter,
+                 subgroup_expressions):
+        ''' Instantiates a session group object 
+        
+        Arguments:
+        api -- object used to enact any changes/modifications
+        name -- the name defined for the Session Group
+        session_type -- the enumeration used to describe the type of session
+        is_system -- describes whether the Session Group is user-modifiable
+        session_filter -- definition by which the group is populated
+        subgroup_expressions -- criteria for any further session organization
+        '''
 
         self.api = api
         self.name = name
         self.session_type = session_type
+
+        self.is_system = is_system
+        self.session_filter = session_filter
+        self.subgroup_expressions = subgroup_expressions
 
     def modify(self, **kwargs):
         ''' Alter the details or settings of the session group '''
